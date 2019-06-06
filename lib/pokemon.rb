d640b0a292525051be9cc25a1c431cf7926f0896
blessed-scope-5001 <roxanne.harris@yale.edu>
2019-06-06 17:21:40 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -20,7 +20,7 @@ class Pokemon
     WHERE id=?
     SQL
     row = db.execute(sql, id).first
-    Pokemon.new(id: row[0], name: row[1], type: row[2], db: db)
+    Pokemon.new(id: row[0], name: row[1], type: row[2], db: db, hp: row[3])
   end
 
   def alter_hp(hp, db)
