97900c94a47f4163c6a2365076913a22e777bf67
blessed-scope-5001 <roxanne.harris@yale.edu>
2019-06-06 17:14:29 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -24,4 +24,12 @@ class Pokemon
     Pokemon.new(id: row[0], name: row[1], type: row[2], db: db)
   end
 
+  def alter_hp(hp, db)
+    sql = <<-SQL
+    UPDATE pokemon SET hp=?
+    SQL
+    row = db.execute(sql, id).first
+    Pokemon.new(id: row[0], name: row[1], type: row[2], db: db)
+  end
+
 end
