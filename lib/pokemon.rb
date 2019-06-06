4fc3c655aed8d4819b2ebeef637f483240661c22
blessed-scope-5001 <roxanne.harris@yale.edu>
2019-06-06 17:07:27 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -7,12 +7,12 @@ class Pokemon
     @hp = 60
   end
 
-  def self.save(name, type, db)
+  def self.save(name, type, hp, db)
     sql = <<-SQL
     INSERT INTO pokemon(name, type, hp)
     VALUES(?,?,?)
     SQL
-    db.execute(sql, name, type)
+    db.execute(sql, name, type, hp)
   end
 
   def self.find(id, db)
