cad43ce0d7788923d555dc97a61b2991477d46d6
blessed-scope-5001 <roxanne.harris@yale.edu>
2019-06-06 17:07:48 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -10,7 +10,7 @@ class Pokemon
   def self.save(name, type, db)
     sql = <<-SQL
     INSERT INTO pokemon(name, type, hp)
-    VALUES(?,?,?)
+    VALUES(?,?,60)
     SQL
     db.execute(sql, name, type, db)
   end
