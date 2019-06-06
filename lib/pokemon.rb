815aa614fdebf3ee8163efe16697c80de20c7d6a
blessed-scope-5001 <roxanne.harris@yale.edu>
2019-06-06 16:47:22 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,18 @@
 class Pokemon
+
+  attr_accessor :id, :name, :type, :db
+
+  def initialize(attributes)
+    attributes.each {|key, value| self.send("#{key}=", value)}
+  end
+
+  def self.save(name, type, db)
+    sql = <<-SQL
+    INSERT INTO pokemon(name, type)
+    VALUES(?,?)
+    SQL
+    db.execute(sql, name, type)
+  end
+
+
 end
